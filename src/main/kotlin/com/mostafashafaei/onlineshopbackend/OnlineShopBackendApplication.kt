21892a3e802e6c9df14c63aad249526af307e774
Mostafa <mostafa@pop-os.lan>
2026-02-13 15:53:53 +0330

    chore: initial commit

---
+++ src/main/kotlin/com/mostafashafaei/onlineshopbackend/OnlineShopBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.mostafashafaei.onlineshopbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineShopBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineShopBackendApplication>(*args)
+}
